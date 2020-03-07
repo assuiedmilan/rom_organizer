7da4facc7320ba13f1aa98c2ba0b7a670625b978
Milan Assuied <milan.assuied@opal-rt.com>
2020-03-07 08:13:31 -0500

    Add code

---
+++ organizer/parser/game_list_parser.py
@@ -0,0 +1,94 @@
+import collections
+import os
+import re
+from xml.etree import ElementTree
+
+
+class GameListParser:
+    GAMELIST_FILE = 'gamelist.xml'
+
+    GAME_ID = "id"
+    GAME_KEY = "game"
+    GENRE_KEY = "genre"
+    PATH_KEY = "path"
+    ROOT_KEY = "root"
+    NAME_KEY = "name"
+    NO_TEXT = "Undefined_text"
+
+    game_map = collections.OrderedDict()
+
+    def __init__(self, gamelist_path):
+        self.root = gamelist_path
+        self.gamelist = os.path.join(gamelist_path, self.GAMELIST_FILE)
+        self.parsed_gamelist = None
+        self.__parse()
+
+    def get_parsed_games(self):
+        return self.game_map
+
+    def reparse(self):
+        self.__parse()
+
+    def __parse(self):
+        self.parsed_gamelist = ElementTree.parse(self.gamelist)
+        self.__process_all_games()
+
+    def __get_all_games(self):
+        return self.parsed_gamelist.findall(self.GAME_KEY)
+
+    def __get_game_id(self, game):
+        return game.get(self.GAME_ID)
+
+    def __game_is_valid(self, game):
+        return self.__get_game_id(game) is not None and self.__get_game_id(game) is not "0"
+
+    def __get_game_genre(self, game):
+        match_any_slashes_or_space = re.compile(r'(?:\s+|\\+|/+)')
+        match_any_carret = re.compile(r'-+')
+
+        genre = self.__process_game_child_value(game, self.GENRE_KEY)
+
+        genre = re.sub(match_any_slashes_or_space, '-', genre)
+        genre = re.sub(match_any_carret, '-', genre)
+
+        return genre if genre is not self.NO_TEXT else "Unclassified"
+
+    def __get_game_path(self, game):
+        return self.__process_game_child_value(game, self.PATH_KEY)
+
+    def __get_game_name(self, game):
+        return self.__process_game_child_value(game, self.NAME_KEY)
+
+    def __process_all_games(self):
+        for game in self.__get_all_games():
+            self.__process_game_nodes(game)
+
+    def __process_game_nodes(self, game):
+        if self.__game_is_valid(game):
+            details = {self.GENRE_KEY: self.__get_game_genre(game),
+                       self.PATH_KEY: self.__get_game_path(game),
+                       self.ROOT_KEY: self.root}
+
+            self.game_map[self.__get_game_name(game)] = details
+
+    def __process_game_child_value(self, game, key):
+        game_child = game.find(key)
+
+        if game_child is None or game_child.text is None:
+            return self.NO_TEXT
+        else:
+            return game_child.text
+
+    def __str__(self):
+        text_output = []
+
+        for game_id, game in self.game_map.items():
+            text_output.append("Game " + game_id + " has properties:")
+            for key, value in game.items():
+                if value is None:
+                    text_output.append("ERROR Undefined value for " + key)
+                else:
+                    text_output.append(key + ": " + value)
+            text_output.append("\n")
+
+        return "\n".join(text_output)
