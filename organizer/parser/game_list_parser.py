8cc05523a27c3e83fcdf127e6d628168fd7e34fa
Milan Assuied <sharp_andreanne@hotmail.com>
2020-03-14 18:18:40 -0400

    Add matching function names -> games

--- organizer/parser/game_list_parser.py
+++ organizer/parser/game_list_parser.py
@@ -1,6 +1,5 @@
 import os
 import re
-import traceback
 
 from lxml import etree
 
@@ -20,16 +19,16 @@ class GameListParser(object):
     NO_TEXT = "Undefined_text"
 
     def __init__(self, gamelist_path):
-        self.__root = gamelist_path
-        self.__gamelist = os.path.join(gamelist_path, self.GAMELIST_FILE)
-        self.__parsed_gamelist = None
-        self.__files_to_parse = []
+        self.root = gamelist_path
+        self.gamelist = os.path.join(gamelist_path, self.GAMELIST_FILE)
+        self.parsed_gamelist = None
+        self.files_to_parse = []
 
     def get_all_games(self):
-        if self.__parsed_gamelist is None:
+        if self.parsed_gamelist is None:
             self.__parse()
 
-        return self.__parsed_gamelist.findall(self.GAME_KEY)
+        return self.parsed_gamelist.findall(self.GAME_KEY)
 
     def get_game_id(self, game):
         return game.get(self.GAME_ID)
@@ -55,18 +54,33 @@ class GameListParser(object):
         return self.get_game_id(game) is not None and self.get_game_id(game) is not "0"
 
     def __process_all_files_to_parse(self):
-        for root, dirs, names in os.walk(self.__root):
+        for root, dirs, names in os.walk(self.root):
             for name in names:
-                self.__files_to_parse.append(name)
+                self.files_to_parse.append(name)
+                self.get_game_node_from_game_file(name)
 
     # noinspection PyBroadException
     def __parse(self):
         parser = etree.XMLParser(remove_blank_text=True)
 
         try:
-            self.__parsed_gamelist = etree.parse(self.__gamelist, parser)
+            self.parsed_gamelist = etree.parse(self.gamelist, parser)
+            self.__process_all_files_to_parse()
         except Exception as something_happened:
             ExceptionPrinter.print_exception(something_happened)
+
+    def get_game_node_from_game_file(self, name):
+
+        game_node = None
+
+        for game in self.get_all_games():
+            path = self.get_game_path(game)
+            _, file_name = os.path.split(path)
+
+            if file_name == name:
+                game_node = game
+
+        return game_node
 
     def __process_game_child_value(self, game, key):
         game_child = game.find(key)
