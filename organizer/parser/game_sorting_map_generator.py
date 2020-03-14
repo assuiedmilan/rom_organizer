6528b373ab3dcff3735e5040d5c3eedf1941d675
Milan Assuied <sharp_andreanne@hotmail.com>
2020-03-14 19:14:38 -0400

    Parse using file names instead of game nodes

--- organizer/parser/game_sorting_map_generator.py
+++ organizer/parser/game_sorting_map_generator.py
@@ -20,17 +20,19 @@ class GameSortingMapGenerator(object):
 
     def process_game_map(self):
 
-        for game in self.game_parser.get_all_games():
-            self.__process_games_details(game)
+        for file_name in self.game_parser.get_all_files():
+            game = self.game_parser.get_game_node_from_game_file(file_name)
+
+            if game is not None:
+                self.__process_games_details(game)
 
     def __process_games_details(self, game):
 
-        if self.game_parser.is_game_valid(game):
-            details = {self.game_parser.GENRE_KEY: self.__compute_game_genre(game),
-                       self.game_parser.PATH_KEY: self.game_parser.get_game_path(game),
-                       self.game_parser.ROOT_KEY: self.gamelist_path}
+        details = {self.game_parser.GENRE_KEY: self.__compute_game_genre(game),
+                   self.game_parser.PATH_KEY: self.game_parser.get_game_path(game),
+                   self.game_parser.ROOT_KEY: self.gamelist_path}
 
-            self.game_map[self.game_parser.get_game_name(game)] = details
+        self.game_map[self.game_parser.get_game_name(game)] = details
 
     def __compute_game_genre(self, game):
 
