d29e2c5149841c552e469dfa7e40483a71fa48d5
Milan Assuied <milan.assuied@opal-rt.com>
2020-03-15 13:48:39 -0400

    Compute new location relative to original path

--- organizer/games/game_organizer.py
+++ organizer/games/game_organizer.py
@@ -1,33 +1,55 @@
 import os
 import shutil
 
+from organizer.tools.exception_tools import ExceptionPrinter
+
 
 class GameOrganizer:
 
-    def __init__(self):
-        pass
+    def __init__(self, game):
+        self.game = game
 
-    def move_game_files(self, list_of_games):
+    @classmethod
+    def move_game_files(cls, list_of_games):
 
         for game in list_of_games:
-            self.__move_game_file(game)
+            organizer = GameOrganizer(game)
 
-    @staticmethod
-    def __move_game_file(game):
+            organizer.__move_game_file()
 
-        current_path = game.get_current_path()
-        file_name = os.path.basename(os.path.normpath(current_path))
-        target_root = os.path.normpath(os.path.join(game.get_root_path(), game.get_genre()))
-        target_path = os.path.normpath(os.path.join(target_root, file_name))
+
+    def __move_game_file(self):
+
+        current_path, target_root, target_path = self.get_paths()
 
         if current_path is not target_path and os.path.isfile(current_path):
 
             try:
+
                 if not os.path.isdir(target_root): os.makedirs(target_root)
                 shutil.move(current_path, target_path)
+
             except Exception as something_happened:
-                print("Failed to move file\n  from " + current_path + "\n  to " + target_path)
-                print(something_happened)
+                ExceptionPrinter.print_exception(something_happened)
+
+    def get_paths(self):
+        current_path = self.game.get_current_path()
+        file_name = os.path.basename(os.path.normpath(current_path))
+        target_root = os.path.normpath(os.path.join(self.game.get_root_path(), self.game.get_genre()))
+        target_path = os.path.normpath(os.path.join(target_root, file_name))
+
+        return current_path, target_root, target_path
+
+    def compute_new_game_location(self):
+
+        current_path, _, target_path = self.get_paths()
+
+        original_folder = os.path.split(current_path)[0]
+        relative_path = os.path.relpath(target_path, original_folder)
+        unix_relative_path = './' + relative_path.replace(os.path.sep, '/')
+
+        return os.path.normpath(os.path.join(original_folder, unix_relative_path))
+
 
 
 
