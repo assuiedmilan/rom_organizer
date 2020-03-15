e26ddb26050413111f47d1d50ab6455808bf3382
Milan Assuied <milan.assuied@opal-rt.com>
2020-03-15 15:03:34 -0400

    Cleaning
    # Conflicts:
    #	main.py

--- organizer/games/game_organizer.py
+++ organizer/games/game_organizer.py
@@ -7,12 +7,8 @@ from organizer.tools.exception_tools import ExceptionPrinter
 
 class GameOrganizer:
 
-    def __init__(self, game):
-        self.game = game
-        self.parser = None
-
-    @classmethod
-    def move_game_files(cls, list_of_games, gamelist_folder):
+    @staticmethod
+    def move_game_files(list_of_games, gamelist_folder):
 
         parser = GameListParser(gamelist_folder)
         parser.parse()
@@ -25,6 +21,13 @@ class GameOrganizer:
 
         parser.write_document()
 
+    def register_parser(self, parser):
+        self.parser = parser
+
+    def __init__(self, game):
+        self.game = game
+        self.parser = None
+
     def get_game_node(self):
         if self.parser is not None:
             game_name = self.game.get_filename()
@@ -32,16 +35,13 @@ class GameOrganizer:
 
         return None
 
-    def register_parser(self, parser):
-        self.parser = parser
-
     def __update_game_node(self):
         node = self.get_game_node()
-        node.find(GameListParser.PATH_KEY).text = self.compute_new_game_location()
+        node.find(GameListParser.PATH_KEY).text = self.__compute_new_game_location()
 
     def __move_game_file(self):
 
-        current_path, target_root, target_path = self.get_paths()
+        current_path, target_root, target_path = self.__get_paths()
 
         if current_path is not target_path and os.path.isfile(current_path):
 
@@ -53,7 +53,7 @@ class GameOrganizer:
             except Exception as something_happened:
                 ExceptionPrinter.print_exception(something_happened)
 
-    def get_paths(self):
+    def __get_paths(self):
         current_path = self.game.get_current_path()
         file_name = os.path.basename(os.path.normpath(current_path))
 
@@ -62,13 +62,13 @@ class GameOrganizer:
 
         return current_path, target_root, target_path
 
-    def compute_new_game_location(self):
+    def __compute_new_game_location(self):
 
         unix_relative_path = []
 
         if self.parser is not None:
 
-            _, _, target_path = self.get_paths()
+            _, _, target_path = self.__get_paths()
 
             root = self.parser.get_root()
 
