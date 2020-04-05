9e273f224470fb02b92949e6ebe3bb6eee58dd2c
Milan Assuied <milan.assuied@opal-rt.com>
2020-04-04 22:06:09 -0400

    Try to improve the test (write will fail)

--- tests_organizer/tools/test_xml_files_operations.py
+++ tests_organizer/tools/test_xml_files_operations.py
@@ -1,18 +1,27 @@
 import mock
+import pytest
 
 from organizer.tools.xml_files_operations import XmlFilesOperations
 
 FILE_NAME = "toto.xml"
 
+@pytest.fixture()
+def mock_parser():
+    parser = mock.patch('organizer.tools.xml_files_operations.etree.XMLParser').start()
+    with mock.patch('organizer.tools.xml_files_operations.etree.XMLParser', return_value=parser):
+        yield parser
+
+    parser.stop()
+
 @mock.patch('organizer.tools.xml_files_operations.etree.ElementTree')
 def test_write(mock_document):
 
     XmlFilesOperations.write(mock_document, FILE_NAME)
-    mock_document.write.assert_called()
+    mock_document.write.assert_called_with(FILE_NAME, pretty_print=True)
 
 @mock.patch('organizer.tools.xml_files_operations.etree')
-def test_parse(mock_xml):
+def test_parse(mock_xml, mock_parser):
 
     XmlFilesOperations.parse(FILE_NAME)
-    mock_xml.parse.assert_called()
+    mock_xml.parse.assert_called_with(FILE_NAME, mock_parser)
 
