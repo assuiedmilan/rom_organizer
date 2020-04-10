f4934e136402374b69ab1f06f820f8295b124490
Milan Assuied <milan.assuied@opal-rt.com>
2020-04-10 18:53:38 -0400

    Fix test

--- tests_organizer/tools/test_xml_files_operations.py
+++ tests_organizer/tools/test_xml_files_operations.py
@@ -1,17 +1,8 @@
 import mock
-import pytest
 
 from organizer.tools.xml_files_operations import XmlFilesOperations
 
 FILE_NAME = "toto.xml"
-
-@pytest.fixture()
-def mock_parser():
-    parser = mock.patch('organizer.tools.xml_files_operations.etree.XMLParser').start()
-    with mock.patch('organizer.tools.xml_files_operations.etree.XMLParser', return_value=parser):
-        yield parser
-
-    parser.stop()
 
 @mock.patch('organizer.tools.xml_files_operations.etree.ElementTree')
 def test_write(mock_document):
@@ -19,9 +10,10 @@ def test_write(mock_document):
     XmlFilesOperations.write(mock_document, FILE_NAME)
     mock_document.write.assert_called_with(FILE_NAME, pretty_print=True)
 
+@mock.patch('organizer.tools.xml_files_operations.etree.XMLParser')
 @mock.patch('organizer.tools.xml_files_operations.etree')
-def test_parse(mock_xml, mock_parser):
+def test_parse(mock_etree, mock_xml_parser):
 
     XmlFilesOperations.parse(FILE_NAME)
-    mock_xml.parse.assert_called_with(FILE_NAME, mock_parser)
+    mock_etree.parse.assert_called_with(FILE_NAME, mock_xml_parser())
 
