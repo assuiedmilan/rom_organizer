2f5ac9455b3959aa6c92bea9bccfc65644c039a1
Milan Assuied <milan.assuied@opal-rt.com>
2020-04-18 19:37:22 -0400

    First genre alias generator test

---
+++ tests_organizer/common/genre_associations.py
@@ -0,0 +1,19 @@
+XML_DOCUMENT = """<?xml version="1.0"?>
+<root>
+  <genre_associations text="Defines aliases between games genres for folders organization">
+    <genre genre="action" alias="Action"/>
+    <genre genre="Action" alias="Action"/>
+    <genre genre="action-action" alias="Action"/>
+    <genre genre="Action-Action" alias="Action"/>
+    <genre genre="action-action-adventure" alias="Adventure"/>
+    <genre genre="Action-Action-Adventure" alias="Adventure"/>
+    <genre genre="action-action-adventure-adventure" alias="Adventure"/>
+    <genre genre="action-action-adventure-platform" alias="Platform"/>
+    <genre genre="Action-Action-Adventure-Platform" alias="Platform"/>
+    <genre genre="Action-Action-Breakout-games" alias="Action"/>
+    <genre genre="action-action-breakout-games" alias="Action"/>
+    <genre genre="action-action-labyrinth" alias="Labyrinth"/>
+    <genre genre="Action-Action-Labyrinth" alias="Labyrinth"/>
+  </genre_associations>
+</root>
+"""
--- tests_organizer/parser/test_genre_aliases_generator.py
+++ tests_organizer/parser/test_genre_aliases_generator.py
@@ -1,9 +1,39 @@
+from lxml import etree
+
 import mock
 import pytest
 
+from organizer.parser.genre_aliases_generator import GenreAliasesGenerator
+from tests_organizer.common import genre_associations
+from tests_organizer.common.fixtures import root_values
+
+PARSED_DOCUMENT = etree.fromstring(genre_associations.XML_DOCUMENT)
+IS_SINGLE_FOLDER = [True, False]
+REPLACEMENT_LIST = [None, ['Action', 'Platform'], ['Platform', 'Action']]
+
+@pytest.fixture(params = IS_SINGLE_FOLDER)
+def single_folder(request):
+    yield request.param
+
+@pytest.fixture(params = REPLACEMENT_LIST)
+def replacement_list(request):
+    yield request.param
 
 @pytest.fixture()
-def generator():
-    with mock.patch('os.path.isfile') as is_file:
-        is_file.return_value = False
+def generator(root_values, single_folder, replacement_list):
+    yield GenreAliasesGenerator(root_values, single_folder, replacement_list)
 
+@pytest.fixture()
+def ready_generator(generator):
+    with mock.patch('os.path.isfile') as is_file:
+        with mock.patch('organizer.tools.xml_files_operations.XmlFilesOperations.parse') as parser:
+            parser.return_value = PARSED_DOCUMENT
+            is_file.return_value = True
+            yield generator
+
+@pytest.mark.parametrize('generator_parameter', [
+    pytest.lazy_fixture('ready_generator'),
+    pytest.param(pytest.lazy_fixture('generator'), marks=pytest.mark.xfail(reason="Document does not exists", strict=True))
+])
+def test_document_exist(generator_parameter):
+    assert generator_parameter.document_exists()
